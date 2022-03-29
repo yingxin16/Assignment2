1d77dbe2cd501cfe63981fde120061df8465fa46
yingxin16 <102541899+yingxin16@users.noreply.github.com>
2022-03-30 00:52:17 +0800

    Model java file

---
+++ StudentModel.java
@@ -0,0 +1,39 @@
+public class StudentModel {
+	
+	int id;
+	String name;
+	String department;
+	double mathMark;
+	double scienceMark;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public double getMathMark() {
+		return mathMark;
+	}
+	public void setMathMark(double mathMark) {
+		this.mathMark = mathMark;
+	}
+	public double getScienceMark() {
+		return scienceMark;
+	}
+	public void setScienceMark(double scienceMark) {
+		this.scienceMark = scienceMark;
+	}
+
+}
